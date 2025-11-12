b3652cc44c20ddef014b1ae1c912dc428e77f8f8
Julian54628 <julian.arenas-a@mail.escuelaing.edu.co>
2025-11-12 09:21:57 -0500

    creacion del repositorio

---
+++ src/main/java/ECIEXPRESS/kappa_Pedidos/Orders/_BackEnd/Kappa_Orders_BackEnd/KappaOrdersBackEndApplication.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.kappa_Pedidos.Orders._BackEnd.Kappa_Orders_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaOrdersBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaOrdersBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ECIEXPRESS/kappa_Pedidos/Orders/_BackEnd/Kappa_Orders_BackEnd/KappaOrdersBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.kappa_Pedidos.Orders._BackEnd.Kappa_Orders_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaOrdersBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
