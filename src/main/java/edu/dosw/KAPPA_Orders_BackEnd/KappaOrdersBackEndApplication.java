842b20571ca6294b0d9369870d7534ea4666fff6
Julian54628 <julian.arenas-a@mail.escuelaing.edu.co>
2025-11-12 20:31:26 -0500

    repositorio actualizado con nuevos cambios

---
+++ src/main/java/edu/dosw/KAPPA_Orders_BackEnd/KappaOrdersBackEndApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.KAPPA_Orders_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaOrdersBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaOrdersBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/KAPPA_Orders_BackEnd/KappaOrdersBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.KAPPA_Orders_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaOrdersBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
