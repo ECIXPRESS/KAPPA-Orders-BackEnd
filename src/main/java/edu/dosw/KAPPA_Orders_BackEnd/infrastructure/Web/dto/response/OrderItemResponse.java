5198e054ed380ac7a52198a60ae37b6292080cba
Julian54628 <julian.arenas-a@mail.escuelaing.edu.co>
2025-12-03 21:48:14 -0500

    correccion packetes infraestructure

--- src/main/java/edu/dosw/KAPPA_Orders_BackEnd/infrastructure/Web/dto/response/OrderItemResponse.java
+++ src/main/java/edu/dosw/KAPPA_Orders_BackEnd/infrastructure/Web/dto/response/OrderItemResponse.java
@@ -1,4 +1,4 @@
-package edu.dosw.KAPPA_Orders_BackEnd.Infrastructure.Web.dto.response;
+package edu.dosw.KAPPA_Orders_BackEnd.infrastructure.Web.dto.response;
 
 import edu.dosw.KAPPA_Orders_BackEnd.Domain.Model.OrderType;
 
